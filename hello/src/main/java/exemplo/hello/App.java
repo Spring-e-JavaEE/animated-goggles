5ffe5fe44dbae713c55dea3822e7b7fd12537ecf
leonardo carlos <lcarlos@cipucrs.pucrs.br>
2018-05-25 15:32:11 -0300

    lol

--- hello/src/main/java/exemplo/hello/App.java
+++ hello/src/main/java/exemplo/hello/App.java
@@ -4,10 +4,15 @@ package exemplo.hello;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+
+		System.out.println(returnHello());
+	}
+
+	public static String returnHello() {
+		return "Hello World!";
+	}	
+	
 }
